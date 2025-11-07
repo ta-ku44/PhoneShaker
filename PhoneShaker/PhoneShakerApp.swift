9d102578f4e5a69f38d4528325dc677ae789a834
ta9to <213548904+ta9to-a@users.noreply.github.com>
2025-11-07 11:29:18 +0900

    Initial Commit

---
+++ PhoneShaker/PhoneShakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneShakerApp.swift
+//  PhoneShaker
+//
+//  Created by たっくん on 2025/11/07.
+//
+
+import SwiftUI
+
+@main
+struct PhoneShakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
